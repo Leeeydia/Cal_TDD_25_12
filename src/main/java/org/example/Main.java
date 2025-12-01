ae6709dff65a2bf9fa2d316d2e1a232d999275f3
Leeeydia <leeeydia@gmail.com>
2025-12-01 12:44:53 +0900

    수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,7 @@
 package org.example;
-// 재귀
-// TDD
-// 계산기 -> 알고리즘 문제 해결 능력
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(
-                "Hello World!"
-        );
+        System.out.println(123);
     }
 }
