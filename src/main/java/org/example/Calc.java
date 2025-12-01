3bf5a305be04ff09fe2db821246d1daeae0a2107
Leeeydia <leeeydia@gmail.com>
2025-12-01 17:32:57 +0900

    12/01 다항 계산식 수업

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,11 +1,30 @@
 package org.example;
 
 public class Calc {
-    public static int run(String exp){
 
-    String[] bits = exp.split(" \\+ ");
-    int a = Integer.parseInt(bits[0]);
-    int b = Integer.parseInt(bits[1]);
-    return a+b;
+    public static int run(String exp) {
+
+        System.out.println("exp1 : " + exp);
+
+        exp = exp.replace("- ", "+ -");
+
+        System.out.println("exp2 : " + exp);
+
+        String[] bits = exp.split(" \\+ ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+        int c = 0;
+
+        if (bits.length > 2) {
+            c = Integer.parseInt(bits[2]);
+        }
+
+        return a + b + c;
+
+
+//        throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
     }
+
+
 }
