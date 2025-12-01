2c08b1b83217485cabc1e7323a5b0d928020d4d4
Leeeydia <leeeydia@gmail.com>
2025-12-01 11:48:39 +0900

    1 + 1

---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalcTest {
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    public void test1() {
+
+        assertThat(Calc.run(" 1 + 1 ")).isEqualTo(2);
+
+
+    }
+}
