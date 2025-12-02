157fd0f3666fa85c5306ff646577e11395ce36d8
Leeeydia <leeeydia@gmail.com>
2025-12-02 14:15:47 +0900

    12/02 다항식 계산 내 풀이 2

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,23 +1,83 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Calc {
 
     public static int run(String exp) {
 
-        System.out.println("exp1 : " + exp);
+        if (!exp.contains(" ")) {
+            return Integer.parseInt(exp);
+        }
 
-        exp = exp.replace("- ", "+ - ");
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = exp.contains("+");
 
-        System.out.println("exp2 : " + exp);
+        boolean needToCompound = needToPlus && needToMulti;
 
-        String[] bits = exp.split(" \\+ ");
+        exp = exp.replace("- ", "+ -");
 
-       int sum = 0;
-       for (String bit : bits) {
-           sum += Integer.parseInt(bit);
-       }
-       return sum;
-//        throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
+        if (needToCompound) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+//1
+//            StringBuilder sb = new StringBuilder();
+//
+//            for (int i = 0; i < bits.length; i++) {
+//                int result = Calc.run(bits[i]);
+//                sb.append(result);
+//
+//                // 마지막 요소가 아니면 " + " 추가
+//                if (i < bits.length - 1) {
+//                    sb.append(" + ");
+//                }
+//            }
+//
+//            String newExp = sb.toString();
+
+//2
+//            String newExp = "";
+//
+//            for (int i = 0; i < bits.length; i++) {
+//                int result = Calc.run(bits[i]);
+//                newExp += result;
+//
+//                if (i < bits.length - 1) {
+//                    newExp += " + ";
+//                }
+//            }
+
+            return run(newExp);
+        }
+
+        if (needToPlus) {
+            String[] bits = exp.split(" \\+ ");
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        } else if (needToMulti) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+
+        throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
     }
 
 
