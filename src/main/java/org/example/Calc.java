a3a73d45dcd5c8fccb14d4d09ea292c9f7dfd278
Leeeydia <leeeydia@gmail.com>
2025-12-02 16:53:03 +0900

    12/02 여러겹의 괄호 제거 문제풀이

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -7,17 +7,20 @@ public class Calc {
 
     public static int run(String exp) {
 
+        // 괄호 제거
+        exp = stripOuterBrackets(exp);
+
+        // 그냥 숫자만 들어올 경우 바로 리턴
         if (!exp.contains(" ")) {
             return Integer.parseInt(exp);
         }
-        exp = exp.replace("- ", "+ -");
 
-        boolean needToMulti = exp.contains("*");
-        boolean needToPlus = exp.contains("+");
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
 
         boolean needToCompound = needToPlus && needToMulti;
 
-
+        exp = exp.replace("- ", "+ -");
 
         if (needToCompound) {
             String[] bits = exp.split(" \\+ ");
@@ -55,5 +58,24 @@ public class Calc {
         throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
     }
 
+    private static String stripOuterBrackets(String exp) {
+
+        int outerBracketsCount = 0;
+
+        while (exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')') {
+            outerBracketsCount++;
+        }
+
+        if (outerBracketsCount == 0) return exp;
+
+        return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
+    }
+
 
 }
+
+// while 문으로 반복 하면서 첫번째 (과 마지막)를 빼내는데
+// ++로 반복한다. -> () 갯수가 늘어나도 상관 없음.
+// index 공부. ..
+
+// 끝에가 괄호가 아니다
